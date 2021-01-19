db17d0c3ecf78ecfe441f9d71f405c6245219870
Wahyu <al.arudei01@gmail.com>
2021-01-19 11:08:54 +0700

    Added premake5

---
+++ premake5.lua
@@ -0,0 +1,33 @@
+project "ImGui"
+	kind "StaticLib"
+	language "C"
+
+	targetdir ("bin" .. outputdir .. "%{prj.name}")
+	objdir ("bin-int" .. outputdir .. "%{prj.name}")
+
+	files
+	{
+        "imconfig.h",
+        "imgui.h",
+        "imgui.cpp",
+        "imgui_draw.cpp",
+        "imgui_tables.cpp",
+        "imgui_internal.h",
+        "imgui_widgets.cpp",
+        "imstb_rectpack.h",
+        "imstb_textedit.h",
+        "imstb_truetype.h",
+        "imgui_demo.cpp",
+        "backends/*.cpp",
+        "backends/*.h",
+	}
+
+
+	filter "system:windows"
+		systemversion "latest"
+        cppdialect "C++17"
+		staticruntime "On"
+
+
+	filter { "system:windows", "configurations:Release" }
+		buildoptions "/MT"
